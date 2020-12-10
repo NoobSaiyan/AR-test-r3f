af8c618fc823e7ad2212e8f0445c68ffed87f049
NoobSaiyan <ms19961998@gmail.com>
2020-12-10 19:50:39 +0530

    removing unused text

--- src/App.tsx
+++ src/App.tsx
@@ -1,5 +1,5 @@
 import React, { useState, useCallback } from 'react'
-import { Hover, Select, DefaultXRControllers, ARCanvas } from '@react-three/xr'
+import { Select, DefaultXRControllers, ARCanvas } from '@react-three/xr'
 import { Text } from '@react-three/drei/Text'
 
 import './App.css'
@@ -15,7 +15,6 @@ function Box({ color, size, scale, children, ...rest }: any) {
 }
 
 function Button(props: any) {
-  const [hover, setHover] = useState(false)
   const [color, setColor] = useState<any>('blue')
 
   const onSelect = useCallback(() => {
@@ -24,25 +23,23 @@ function Button(props: any) {
 
   return (
     <Select onSelect={onSelect}>
-      <Hover onChange={setHover}>
-        <Box
-          position={[0, -3, -1]}
-          color={color}
-          scale={hover ? [0.6, 0.6, 0.6] : [0.5, 0.5, 0.5]}
-          size={[0.4, 0.1, 0.1]}
-          {...props}
+      <Box
+        position={[0, -3, -1]}
+        color={color}
+        scale={[0.5, 0.5, 0.5]}
+        size={[0.4, 0.1, 0.1]}
+        {...props}
+      >
+        <Text
+          position={[0, 0, 0.06]}
+          fontSize={0.05}
+          color='#000'
+          anchorX='center'
+          anchorY='middle'
         >
-          <Text
-            position={[0, 0, 0.06]}
-            fontSize={0.05}
-            color='#000'
-            anchorX='center'
-            anchorY='middle'
-          >
-            NoobSaiyan
-          </Text>
-        </Box>
-      </Hover>
+          NoobSaiyan
+        </Text>
+      </Box>
     </Select>
   )
 }
